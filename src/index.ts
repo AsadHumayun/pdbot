28fb55dc3d7d2f70b8220db6d9a7f0bb9ee3f144
Asad <105254706+AsadHumayun@users.noreply.github.com>
2026-01-31 16:09:21 +0000

    fix: events handler

--- src/index.ts
+++ src/index.ts
@@ -18,19 +18,14 @@ client.on('warn', console.warn);
 
 const events = await loadEvents(new URL('events/', import.meta.url));
 
-for (const event of events!) {
-	console.log(event);
-}
-
-// events!.forEach((event) => {
-// 	console.log(event);
-// 	client[event.once ? 'once' : 'on'](event.name, async (...args) => {
-// 		try {
-// 			await event.execute(...args);
-// 		} catch (error) {
-// 			console.error(`Error executing event ${String(event.name)}:`, error);
-// 		}
-// 	});
-// });
+events!.each((event) => {
+	client[event.once ? 'once' : 'on'](event.name, async (...args) => {
+		try {
+			await event.execute(...args);
+		} catch (error) {
+			console.error(`Error executing event ${String(event.name)}:`, error);
+		}
+	});
+});
 
 void client.login(process.env.DISCORD_TOKEN);
--- src/util/loadEvents.ts
+++ src/util/loadEvents.ts
@@ -3,21 +3,30 @@ import type { Events } from 'discord.js';
 import { Collection } from 'discord.js';
 import type { Event } from '../types/Evemt.js';
 
-const events = new Collection<keyof Events, Event>();
+export async function loadEvents(path: URL): Promise<Collection<keyof Events, Event>> {
+	const events = new Collection<keyof Events, Event>();
 
-export async function loadEvents(path: URL) {
 	// eslint-disable-next-line n/no-sync
 	const files = readdirSync(path);
 
 	for (const file of files) {
-		if (!file.endsWith('.js')) return;
+		if (!file.endsWith('.js')) continue;
 
 		console.log(`[INFO] Importing ${path}${file}...`);
 		const event = await import(`${path}${file}`);
-		const { name, once, execute } = event.default;
 
-		console.info(`[INFO] Setting event '${name}'...`);
-		events.set(name, { name, once, execute });
+		try {
+			const { name, once, execute } = event.default;
+			console.info(`[INFO] Setting event '${name}'...`);
+
+			events.set(name, { name, once, execute });
+
+			console.log(`[INFO] Returning events collection with ${events.size} entries.`);
+
+			continue;
+		} catch {
+			continue;
+		}
 	}
 
 	return events;
